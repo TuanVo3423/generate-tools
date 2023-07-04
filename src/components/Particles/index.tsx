60b0a6b14f2c14fd23e083ef7509810b8a90a037
TuanVo3423 <tuanvanvo2003@gmail.com>
2023-07-04 18:44:37 +0700

    feat: optimize and intergrate langchainJS

--- src/components/Particles/index.tsx
+++ src/components/Particles/index.tsx
@@ -5,8 +5,6 @@ import { loadFull } from 'tsparticles';
 
 export const ParticlesComp = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-
     // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
